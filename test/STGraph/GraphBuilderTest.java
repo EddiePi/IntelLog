19509c55e8528ad8b941b0075943fff340534cff
EddiePi <piyidi36@sina.com>
2021-06-19 13:51:33 -0700

    sync with local

--- test/STGraph/GraphBuilderTest.java
+++ test/STGraph/GraphBuilderTest.java
@@ -32,8 +32,8 @@ public class GraphBuilderTest {
         //String graphPath = "/Users/Eddie/gitRepo/log-preprocessor/data/mr-data/mr-map-graph.json";
         String graphPath = "/Users/Eddie/gitRepo/log-preprocessor/data/spark-data/spark-test/debug-level/spark-debug-graph.json";
         STNode root = builder.buildGraph();
-        System.out.println("STOP");
-        //GsonSerializer.writeJSON(root, graphPath);
+        //System.out.println("STOP");
+        GsonSerializer.writeJSON(root, graphPath);
     }
 
 }
